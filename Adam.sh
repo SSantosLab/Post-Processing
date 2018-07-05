b458047381b91a9168cab1a57666b1b19d91a1c6
Nora Sherman <nfshermy@users.noreply.github.com>
2018-07-05 08:59:16 -0500

    Update to except multiple ini; update to improve general functionality

--- Adam.sh
+++ Adam.sh
@@ -9,10 +9,55 @@ echo "Running diffimg_setup.sh"
 echo "diffimg_setup done."
 echo
 
-python run_postproc.py &
+ARRAY=(postproc_416.ini) #list of postproc_SEASON.ini files
 
-sleep 45s
+ELEMENTS=${#ARRAY[@]}
+one=1
 
-python run_checker.py 
+if [$ELEMENTS > $one];
+then
+    echo $ELEMENTS
+    for ((count=0;count<$ELEMENTS;count++));
+    do
+	INI=${ARRAY[${count}]}
+	echo $INI
+	python getSeason.py --ini $INI
+	SEASON=`cat getSeason.txt`
+	echo 'SEASON = '$SEASON
 
-echo "The deed is done."
+	export TOPDIR_SNFORCEPHOTO_IMAGES=${ROOTDIR2}/forcephoto/images/dp${SEASON}
+	export TOPDIR_SNFORCEPHOTO_OUTPUT=${ROOTDIR2}/forcephoto/output/dp${SEASON}
+
+	echo $TOPDIR_SNFORCEPHOTO_IMAGES
+	echo $TOPDIR_SNFORCEPHOTO_OUTPUT
+
+	python run_postproc.py --season $SEASON &
+	
+	sleep 45s
+	
+	python run_checker.py --season $SEASON &
+    done
+    echo "The deed is done."
+
+else
+    echo "Only one .ini."
+    INI=${ARRAY[${0}]}
+    echo $INI
+    python getSeason.py --ini $INI
+    SEASON=`cat getSeason.txt`
+    echo 'SEASON = '$SEASON
+
+    export TOPDIR_SNFORCEPHOTO_IMAGES=${ROOTDIR2}/forcephoto/images/dp${SEASON}
+    export TOPDIR_SNFORCEPHOTO_OUTPUT=${ROOTDIR2}/forcephoto/output/dp${SEASON}
+
+    echo $TOPDIR_SNFORCEPHOTO_IMAGES
+    echo $TOPDIR_SNFORCEPHOTO_OUTPUT
+
+    python run_postproc.py --season $SEASON &
+    
+    sleep 45s
+
+    python run_checker.py --season $SEASON
+
+    echo "The deed is done."
+fi
