0e1f5e03457f5c6ea5621dc1a247a4018624e925
Nora Sherman <nfshermy@users.noreply.github.com>
2018-07-30 16:49:22 -0500

    curate list of exposures based on observing night and propid.

---
+++ getExpWPropIDandNite.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while getopts "n:p" opt
+do case "${opt}" in
+    n)
+        nites=${OPTARG};;
+    p)
+	propid=${OPTARG};;
+
+esac
+done
+
+echo $nites
+
+otherEle="$(echo $nites | tr ',' ' ')"
+
+echo $otherEle
+
+ELEMENTS=${#otherEle[@]}
+
+echo $ELEMENTS
+
+awk '$8=="$propid" {print $0}' exposures.list > outfile.txt
+for i in $otherEle 
+do
+    echo $i
+done
+for (count=0;count<$ELEMENTS;count++);
+do
+    nite=${nites[${count}]}
+    awk '$2 == "nite" {print $0}' outfile.txt >> curatedExposure.list
+done
