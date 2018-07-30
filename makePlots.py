6102149f3fd45be7a307c63edd9c21d4c2d8dfb6
Nora Sherman <nfshermy@users.noreply.github.com>
2018-07-30 16:38:25 -0500

    add efficiency plot

--- makePlots.py
+++ makePlots.py
@@ -120,9 +120,97 @@ def MakeDaPlots(season,ccddf,master,truthplus,fitsname,expnums,mjdtrigger,ml_sco
     shutil.copy(RADECName,'./'+radec)
     print('A img was made!','./'+radec)
     
+
+
+    if not skip:
+        for e in hexex:
+            print e
+            out = ''
+            out = os.path.join(rootdir,str(masdf['nite'].loc[masdf['expnum']==e].values[0]))
+            out = os.path.join(out,str(e))
+            out = os.path.join(out,str(e)+'.out')
+
+            odf = pd.read_table(out,delim_whitespace=True,header=None,names=['expnum','band','ccd','ra1','dec1','ra2','dec2','ra3','dec3','ra4','dec4'])
+
+            odf = odf.drop_duplicates()
+            odf = odf.reset_index(drop=True)
+
+            odf.ix[odf.ra1 > 270., 'ra1'] = odf.ix[odf.ra1 > 270., 'ra1'] - 360.
+            odf.ix[odf.ra2 > 270., 'ra2'] = odf.ix[odf.ra2 > 270., 'ra2'] - 360.
+            odf.ix[odf.ra3 > 270., 'ra3'] = odf.ix[odf.ra3 > 270., 'ra3'] - 360.
+            odf.ix[odf.ra4 > 270., 'ra4'] = odf.ix[odf.ra4 > 270., 'ra4'] - 360.
+
+            ras = np.concatenate((odf['ra1'].tolist(),odf['ra2'].tolist(),odf['ra3'].tolist(),odf['ra4'].tolist()),axis=0)
+    
+            decs = np.concatenate((odf['dec1'].tolist(),odf['dec2'].tolist(),odf['dec3'].tolist(\
+),odf['dec4'].tolist()),axis=0)
+
+            for i in range(len(odf)):
+                ra = odf.ix[i,['ra1','ra2','ra3','ra4']]
+                dec = odf.ix[i,['dec1','dec2','dec3','dec4']]
+                chip = str(odf.ix[i,'ccd'])
+                midra = (max(ra)+min(ra))/2.
+                middec = (max(dec)+min(dec))/2.
+                middle = tuple([midra,middec])
+                cs = zip(ra,dec)
+                cent=(sum([c[0] for c in cs])/len(cs),sum([c[1] for c in cs])/len(cs))
+                cs.sort(key=lambda c: math.atan2(c[1]-cent[1],c[0]-cent[0]))
+                cs.append(cs[0])
+                plt.plot([c[0] for c in cs],[c[1] for c in cs],ls=':',lw=0.5,c='k')
+                plt.annotate(chip, xy=middle, ha='center',va='center',family='sans-serif',fontsize=12,alpha=0.3)
+
+            plt.xlim(min(ras)-0.2,max(ras)+0.2)
+            plt.ylim(min(decs)-0.2,max(decs)+0.2)
+
+
+            pltdf = radecdf.loc[radecdf['EXPNUM'] == e]
+            plt.scatter(pltdf['RA'],pltdf['DEC'],c=pltdf['PHOTPROB'],edgecolor='')
+            plt.clim(0,1)
+            plt.colorbar().set_label('ml_score')
+            hexname = masdf['hex'].loc[masdf['expnum'] == e].values[0]
+            plt.title('Candidate Sky Map: Hex '+str(hexname)+' (Exposure '+str(e)+')')
+            plt.xlabel('RA')
+            plt.ylabel('DEC')
+            plt.tight_layout()
+            plt.savefig(os.path.join(mapdir,'map_'+str(hexname)+'_'+str(e)+'_'+season+'.png'),dpi=200)
+            plt.clf()
+            
+            hexm=os.path.join(mapdir,'map_'+str(hexname)+'_'+str(e)+'_'+season+'.png')
+            hexmap=hexm.split('/')[-1]
+            shutil.copy(hexm,'./'+hexmap)
+            print('A img was made!','./'+hexmap)
+
+
+
     if os.path.isfile('./'+radec) and os.path.isfile('./'+mls):
         stat6=True
     else:
         stat6=False
+
+        
+
+    bins = np.arange(17,25,1)
+
+    fhist, bin_edges = np.histogram(df['MAG'], bins=bins)
+    thist, bin_edges = np.histogram(df1['TRUEMAG'], bins=bins)
+
+
+    plt.xlim(17,25)
+    plt.ylim(0,100)
+    plt.plot(bins[:-1], fhist*100.0/thist, lw=4)
+    plt.scatter(bins[:-1], fhist*100.0/thist, lw=4)
+    plt.title('Efficiency')
+    plt.xlabel('Mag')
+    plt.ylabel('Percent Found')
+    plt.savefig(os.path.join(outdir,'efftest_'+season+'.png'))
+    EffName=os.path.join(outdir,'efftest_'+season+'.png')
+    plt.clf()
+    eff=EffName.split('/')[-1]
+    shutil.copy(EffName,'./'+eff)
+    print("You have and efficiency plot!")
+
+    
+    
+
     
     return stat6,mls,radec
