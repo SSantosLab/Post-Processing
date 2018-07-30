7732395fe4e8bf9460c6f16ef27e16979dfb955a
Nora Sherman <nfshermy@users.noreply.github.com>
2018-07-30 16:57:33 -0500

    Get exposure list

---
+++ getExposureInfo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
+
+setup easyaccess
+setup psycopg2
+python getExposureInfo.py
+
+for band in u g r i z Y
+do
+awk '($6 == "'$band'")' exposures.list > exposures_${band}.list
+done
+
+exit
