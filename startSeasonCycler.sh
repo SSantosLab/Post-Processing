4fc19bc8991f29e0cea63517d500dd96bff56d35
Nora Sherman <nfshermy@users.noreply.github.com>
2018-07-31 08:46:11 -0500

    Update fof Functionality

--- startSeasonCycler.sh
+++ startSeasonCycler.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -f /data/des40.b/data/nsherman/exposures.list ]; then
+    echo "No exposure list. Making one!"
+    . getExposureInfo.sh    
+    sleep 3.5m    
+fi
+
+echo "We have an exposure list!"
+python lenExposureInfo.py
+sleep 5s
 if pidof -x "seasonCycler.sh" >/dev/null; then
     echo "seasonCycler is awake."
 else
